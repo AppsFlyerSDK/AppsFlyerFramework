1a788219f140e197b47cc6c7c1b9de63ccdea630
Golan <golan@Golanss-MacBook-Pro.local>
2016-09-19 16:30:49 +0300

    New Framework configuration

--- AppsFlyerTracker.framework/Headers/AppsFlyerTracker.h
+++ AppsFlyerTracker.framework/Headers/AppsFlyerTracker.h
@@ -2,7 +2,7 @@
 //  AppsFlyerTracker.h
 //  AppsFlyerLib
 //
-//  AppsFlyer iOS SDK v4.5.5
+//  AppsFlyer iOS SDK v4.5.6
 //  29-Jun-2016
 //  Copyright (c) 2013 AppsFlyer Ltd. All rights reserved.
 //
