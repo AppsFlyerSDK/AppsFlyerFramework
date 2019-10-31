75df144f4153625953afe2f8efb7d8460f553288
af_deployment <devop@appsflyer.com>
2019-10-31 14:10:17 +0200

    Jenkins auto build

--- tvOS/AppsFlyerLib.framework/Versions/A/Headers/AppsFlyerTracker.h
+++ tvOS/AppsFlyerLib.framework/Versions/A/Headers/AppsFlyerTracker.h
@@ -2,7 +2,7 @@
 //  AppsFlyerTracker.h
 //  AppsFlyerLib
 //
-//  AppsFlyer iOS SDK 4.11.0 (895)
+//  AppsFlyer iOS SDK 4.11.1 (916)
 //  Copyright (c) 2019 AppsFlyer Ltd. All rights reserved.
 //
 
@@ -320,6 +320,11 @@ typedef enum  {
 
 - (NSString *)phoneNumber UNAVAILABLE_ATTRIBUTE;
 
+/**
+ To disable app's vendor identifier(IDFV), set disableIDFVCollection to true
+ */
+@property(nonatomic) BOOL disableIDFVCollection;
+    
 /**
  Enable the collection of Facebook Deferred AppLinks
  Requires Facebook SDK and Facebook app on target/client device.
