e871ac51a796747b93fa215a62f84ce5dbc09e35
ivan.obodovskyi <ivan.obodovskyi@appsflyer.com>
2023-07-10 17:42:18 +0300

    Small fix

--- Package.swift
+++ Package.swift
@@ -13,6 +13,6 @@ let package = Package(
             name: "AppsFlyerLib",
             url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework/releases/download/6.12.0/AppsFlyerLib.xcframework.zip",
             checksum: "b213c8c89ffb1c3dc9f6566c8a9d3fa3d10c57ceedaa720ebc549505f2da96e8"
-        ),
+        )
     ]
 )
